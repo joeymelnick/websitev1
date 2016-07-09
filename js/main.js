62497f0d93b6871b32282dc99686b2228c41b822
Joey Melnick <jmelnick86@gmail.com>
2016-07-08 22:26:54 -0400

    initial commit

---
+++ js/main.js
@@ -0,0 +1,35 @@
+function invertTwitter(){
+  $('.twitter-black').css('display', 'none');
+  $('.twitter-white').css('display', 'block')
+}
+function invertTwitter2(){
+   $('.twitter-black').css('display', 'block');
+  $('.twitter-white').css('display', 'none')
+}
+
+function invertGithub(){
+  $('.github-black').css('display', 'none');
+  $('.github-white').css('display', 'block')
+}
+function invertGithub2(){
+   $('.github-black').css('display', 'block');
+  $('.github-white').css('display', 'none')
+}
+
+function invertLinked(){
+  $('.linked-black').css('display', 'none');
+  $('.linked-white').css('display', 'block')
+}
+function invertLinked2(){
+   $('.linked-black').css('display', 'block');
+  $('.linked-white').css('display', 'none')
+}
+
+function invertFcc(){
+  $('.fcc-black').css('display', 'none');
+  $('.fcc-white').css('display', 'block')
+}
+function invertFcc2(){
+   $('.fcc-black').css('display', 'block');
+  $('.fcc-white').css('display', 'none')
+}
